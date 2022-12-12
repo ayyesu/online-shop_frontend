b3986460962613266f9044e0dbf5c0e8c98fdf2e
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-12-12 01:20:01 +0000

    Implemented react toolkit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsReducer, { productsFetch } from "./features/productSlice";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+store.dispatch(productsFetch())
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
